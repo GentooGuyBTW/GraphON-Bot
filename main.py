15275efddfa59a94262afbdae73b514b98ee0942
GentooGuy <noreply@github.com>
2025-07-05 11:43:50 +0000

    v. 0.2.9

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 SUPPORT_GROUP_ID = os.getenv("CHAT_ID")
-BOT_VER = "0.2.8" # MODIFY VERSION AFTER COMMIT!
+BOT_VER = "0.2.9" # MODIFY VERSION AFTER COMMIT!
 message_map = {}
 
 
